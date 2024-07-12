1196a1d48f212d3652583fa8cf5e5d3c3d1149a5
p3javier <javii.e.saverroes@gmail.com>
2024-07-12 13:38:23 +0200

    feat: added introductory chat message to the user

--- src/components/chat-area.tsx
+++ src/components/chat-area.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ChatMessage from "./ui/chat-message";
-import { useChatStore } from "@/lib";
+import { EVariant, useChatStore, useJourneyStateStore } from "@/lib";
 import { ScrollArea } from "@/components/ui/scroll-area";
 
 interface ChatAreaProps {
@@ -10,9 +10,24 @@ interface ChatAreaProps {
 const ChatArea: React.FC<ChatAreaProps> = (props) => {
   const { className } = props;
   const messages = useChatStore((state) => state.messages);
+  const { initialFormCompleted } = useJourneyStateStore(
+    (state) => state.journeyState
+  );
+
   return (
     <ScrollArea className={className}>
       <div className="flex flex-col gap-4 my-1 mx-4">
+        {initialFormCompleted && (
+          <ChatMessage
+            message={
+              <>
+                Puedes usar este chat para preguntar cualquier duda que tengas
+                sobre el producto.
+              </>
+            }
+            variant={EVariant.received}
+          />
+        )}
         {messages.map((messageProps, index) => {
           const { message, sender, timestamp, variant } = messageProps;
           return (
